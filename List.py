9daaa9da893a084c053b454a65ac5230c03513ce
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-02-09 19:59:45 +0600

    Create List.py

---
+++ List.py
@@ -0,0 +1,14 @@
+marks = [95, 93, 98, "English", "Math", "CSE"]
+
+# Full list print
+print(marks)
+
+# Using for loop
+for i in marks:
+    print(i)
+
+# # Using While loop
+# i = 0
+# while i<len(marks):
+#     print(marks[i])
+#     i+=1

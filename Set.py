1732ee355b3d247c28e00e43c5821c8e15a82158
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-02-09 20:00:54 +0600

    Create Set.py

---
+++ Set.py
@@ -0,0 +1,9 @@
+# Set stores unique values
+# Set start with {}
+# Set does not have any indexes. So, we cannot print the values of specific indexes like print(marks[0])
+# But you can print values using iterators or loops
+
+marks = {95, 99, 100, 99, 95, 99}
+print(marks)
+for i in marks:
+    print(i)

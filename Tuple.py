fef398606ac85ef2ed8deec5eabf12f84a013607
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-02-09 20:01:52 +0600

    Create Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,9 @@
+# Tuple starts with ()
+# We cannot change values in tuple
+# We cannot perform any addition or deletion operations in tuple.
+
+
+marks = (95, 94, 99, 100, 99, 92, 99)
+print(marks)
+print(marks.count(99))
+print(marks.index(99))

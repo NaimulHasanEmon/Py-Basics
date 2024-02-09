2370584cefcb1d669873ebf01cca0202df9869bd
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-02-09 19:57:54 +0600

    Create Dictonary.py

---
+++ Dictonary.py
@@ -0,0 +1,17 @@
+# We will use dictionary in python when we will be needed to store key and value
+
+marks = {"English": 97, "Math": 100}
+
+# Printing the dictionary
+print(marks)
+
+# Printing the marks of English only
+print(marks["English"])
+
+# Adding a new mark to the dictionary
+marks["Physics"] = 96
+print(marks)
+
+# Updating an existing mark to the dictionary
+marks["Physics"] = 99
+print(marks)

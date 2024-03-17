7529c3a7b42656f3169570e69cd1c6fc584bac70
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-03-17 21:19:07 +0600

    Create Key Value Pairs in Dictionary.py

---
+++ Key Value Pairs in Dictionary.py
@@ -0,0 +1,12 @@
+student = {
+    'name': 'Emon',
+    'age': 23,
+    'subject': {
+        'cse': 97,
+        'math': 95,
+        'phy': 93
+    },
+    'phone': 124
+}
+
+print(student.items())

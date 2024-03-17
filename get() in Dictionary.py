4bd6ab5be423c5062f0812c67fa25cb06222f1ed
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-03-17 21:22:13 +0600

    Create get() in Dictionary.py

---
+++ get() in Dictionary.py
@@ -0,0 +1,14 @@
+# It takes a key and returns a value.
+
+student = {
+    'name': 'Emon',
+    'age': 23,
+    'subject': {
+        'cse': 97,
+        'math': 95,
+        'phy': 93
+    },
+    'phone': 124
+}
+
+print(student.get('name'))

470e68aa2a00d90b6386c0f3560e6840bd17824d
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-03-17 21:26:45 +0600

    Create Difference between s['name] and s.get('name') in Dictionary.py

---
+++ Difference between s['name] and s.get('name') in Dictionary.py
@@ -0,0 +1,13 @@
+student = {
+    'name': 'Emon',
+    'age': 23,
+    'subject': {
+        'cse': 97,
+        'math': 95,
+        'phy': 93
+    },
+    'phone': 124
+}
+
+print(student.get('name2')) # Output: None
+print(student['name2']) # Output: error message
